1fa9bb620542223c4b02671eeeb6fbd2f0a0c78c
liaamg1 <lgustafsson71@gmail.com>
2025-03-04 13:38:36 +0100

    added solution to project

---
+++ unit tests/Software1460_lib/Software1460_lib.cpp
@@ -0,0 +1,10 @@
+// Software1460_lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnSoftware1460lib()
+{
+}
